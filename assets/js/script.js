70a81bfae405d9b1075795f7c34e7524444f0a35
Amadewe <dewevre.amandine@gmail.com>
2019-01-21 21:26:04 +0100

    Fin de l'exercice 6

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ function myFunction() {
 var firstNumber = document.getElementById("firstNumber").value;
 var secondNumber = document.getElementById("secondNumber").value;
 if(isNaN(firstNumber)==false && isNaN(secondNumber)==false){
-  var result = Math.floor(firstNumber) * secondNumber;
+  var result = firstNumber % secondNumber;
   alert(result);
 } else {
   alert ('merci de remplir des chiffres');
