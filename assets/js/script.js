6ad9e271e51ccc9a127f19a9d9840d4d4bb00c06
Amadewe <dewevre.amandine@gmail.com>
2019-01-23 16:19:30 +0100

    Modification de l'exercice avec regex

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
 function myFunction() {
+var regex = new RegExp (/[1-9]/);
+var regex2 = new RegExp (/[0-9]/);
 var firstNumber = document.getElementById("firstNumber").value;
 var secondNumber = document.getElementById("secondNumber").value;
-if(isNaN(firstNumber)==false && isNaN(secondNumber)==false){
-  var result = firstNumber % secondNumber;
+var result = firstNumber % secondNumber;
+if (regex.test(secondNumber)& regex2.test(firstNumber)){
+
   alert(result);
 } else {
-  alert ('merci de remplir des chiffres');
+  alert ('merci de remplir correctement tous les champs');
 }
 }
